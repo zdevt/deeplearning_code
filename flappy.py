42069e04f5c62fec5d6cb6b597d7355fda970c55
zdevt <zhangtao.hawk@qq.com>
2018-05-28 15:10:58 +0800

    add init file

---
+++ flappy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#       FileName:  flappy.py
+#
+#    Description:
+#
+#        Version:  1.0
+#        Created:  2018-05-28 15:01:41
+#  Last Modified:  2018-05-28 15:03:11
+#       Revision:  none
+#       Compiler:  gcc
+#
+#         Author:  zt ()
+#   Organization:
+
+import random
+import sys
+import pygame
+
+from itertools import cycle
+from pygame.locals import *
+
+if __name__ == '__main__':
+    pass
+
